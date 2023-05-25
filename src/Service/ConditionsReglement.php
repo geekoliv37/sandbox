a9b50599a0b60da9365b435ac8e4ff8188de31ea
Olivier <geekoliv37@yahoo.com>
2023-05-26 00:47:14 +0200

    first commit

---
+++ src/Service/ConditionsReglement.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Service;
+
+use DateTime;
+use Exception;
+
+class ConditionsReglement
+{
+    const FM15 = 15;
+    const FM30 = 30;
+    const FM45 = 45;
+
+    public function getConst($condition): int|string
+    {
+        public function addDays($date, $condition)
+    {
+        return $date->add(new \DateInterval('P'.$condition.'D'));
+    }
+        switch ($condition)
+        {
+            case 'FM15' :
+                return ConditionsReglement::FM15;
+            case 'FM30':
+                return ConditionsReglement::FM30;
+            case 'FM45' :
+                return ConditionsReglement::FM45;
+            default:
+                return 'Aucune condition valable';
+        }
+    }
+}
