cad76b1ab77998f97f0980de715d78a9dc8a8a6e
Olivier <geekoliv37@yahoo.com>
2023-05-31 02:07:47 +0200

    commit condition FDM part3

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,21 +3,21 @@
 namespace App\Controller;
 
 use App\Service\ConditionReglementService;
-use ContainerFxGJ40q\getConditionReglementServiceService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use function PHPUnit\Framework\throwException;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
     public function index(ConditionReglementService $conditionReglementService, Request $request): Response
     {
-       // $conditionReglement = new ConditionReglementService();
-      //  dd ($conditionReglement);
-        dd($conditionReglementService->getDateEnd($request->get('date'), $request->get('condition')));
+        $date = $request->query->get('date');
+        $condition = $request->query->get('condition');
+        $dateEnd = $conditionReglementService->getDateEnd( $date, $condition);
+
+        dd($dateEnd);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
--- src/Service/ConditionReglementService.php
+++ src/Service/ConditionReglementService.php
@@ -6,6 +6,7 @@ use DateTime;
 use Exception;
 use Symfony\Component\Validator\Constraints\Date;
 
+
 class ConditionReglementService extends DateTime
 {
 
@@ -15,15 +16,13 @@ class ConditionReglementService extends DateTime
     const CR60NET = array('count'=> 60, 'name' => '60 Jours NET');
     const CR30FDM = array('count'=> 30, 'name' => '30 Jours Fin de mois');
     const CR45FDM = array('count'=> 45, 'name' => '45 Jours Fin de mois');
-    const CR30FDM15 = array('count'=> 30, 'name' => '30 Jours Fin de mois le 15', 'dayFinishedMonth' => 15 );
+    const CR30FDM10 = array('count'=> 30, 'name' => '30 Jours Fin de mois le 10', 'dayEndMonth' => 10 );
+    const CR30FDM15 = array('count'=> 30, 'name' => '30 Jours Fin de mois le 15', 'dayEndMonth' => 15 );
     const FDM15 = array('count'=> 15, 'name' => 'Fin de mois 15 Jours');
     const FDM30 = array('count'=> 30, 'name' => 'Fin de mois 30 Jours');
     const FDM45 = array('count'=> 45, 'name' => 'Fin de mois 45 Jours');
 
-        public function __construct(string $datetime = 'now' , DateTimeZone $timezone = null)
-        {
-            parent::__construct($datetime, $timezone);
-        }
+
 
 
     /**
@@ -36,7 +35,6 @@ class ConditionReglementService extends DateTime
 
     public function getDateEnd(string $date, string $condition): DateTime
     {
-        //  $this->
         $conditionReglement = $condition;   // pour conserver la variable $condition d'origine
         $condition = $this->getNbJourOfConst($condition);
         $date = new DateTime($date);
@@ -48,35 +46,35 @@ class ConditionReglementService extends DateTime
          */
         if (str_starts_with($conditionReglement, 'FDM')) {
             $lastDayOfMonth = $date->format('Y-m-t');
-            var_dump($lastDayOfMonth);
             $date = new DateTime($lastDayOfMonth);
-            var_dump($date);
         }
+        /**
+         * si un jour de fin de mois de la condition de règlement est renseigné dans la constante,
+         * On crée une date de contrôle avec le jour de fin de mois et le mois + année de la variable date
+         */
+        $lastDay = $this->getDayEndMonthConst($conditionReglement);
 
-            $lastDay = $this->getDayEndMonthConst($conditionReglement);
-            if (isset($lastDay))  // si le jour de fin de mois de la condition de règlement est != du mois civil et donc renseigné dans la constante
-            {
-                $checkDateEndOfMonth = $date->format('Y-m-' . $lastDay);
-                if ($checkDateEndOfMonth >= $date) {
-                    $date = new DateTime($checkDateEndOfMonth);
+        if (isset($lastDay) && $lastDay !== 'Aucune condition valable')
+        {
+            var_dump($lastDay);
+            $checkDateEndOfMonth = new DateTime($date->format('Y-m-'.$lastDay)) ;
+            if ($checkDateEndOfMonth >= $date) {    //  si la date de fin de mois de la condition de règlement sur le mois en cours n'est pas passée
 
-                } else {
-                    $date = new DateTime($checkDateEndOfMonth);
-                    $date->modify('+1 month');
-
-
-                    return $date;
-                }
-               // $lastDayOfMonth = $date->format('Y-m-d');
-
+                $date = $checkDateEndOfMonth;    // alors la date de base de calcul = jour de FDM de la condition sur le mois du document
+                var_dump($checkDateEndOfMonth);
+            } else {
+                $date = new DateTime($checkDateEndOfMonth->format('Y-m-d'));
+                $date->modify('+1 month');   // Sinon la date de base de calcul = jour de FDM de la condition sur le mois m+1 du document
                 var_dump($date);
             }
-
-
+        }
 
         return $this->addDays($date, $condition);
 
+        if (preg_match('/CR\d{1,2}FDM',$conditionReglement))
+        {
 
+        }
 
     }
 
@@ -84,9 +82,10 @@ class ConditionReglementService extends DateTime
      * Ajoute des jours à une date
      * @throws Exception
      */
-    public function addDays($date, $condition)
+    public function addDays($date, $condition): DateTime
     {
-        return $date->add(new \DateInterval('P'.$condition.'D'));
+        $date->add(new \DateInterval('P'.$condition.'D'));
+        return $date;
     }
 
     /**
@@ -108,6 +107,8 @@ class ConditionReglementService extends DateTime
                 return ConditionReglementService::CR60NET['count'];
             case 'CR30FDM' :
                 return ConditionReglementService::CR30FDM['count'];
+            case 'CR30FDM10' :
+                return ConditionReglementService::CR30FDM10['count'];
             case 'CR30FDM15' :
                 return ConditionReglementService::CR30FDM15['count'];
             case 'CR45FDM' :
@@ -132,9 +133,10 @@ class ConditionReglementService extends DateTime
     {
         switch ($condition)
         {
+            case 'CR30FDM10' :
+                return ConditionReglementService::CR30FDM10['dayEndMonth'];
             case 'CR30FDM15' :
-                return ConditionReglementService::CR30FDM15['dayFinishedMonth'];
-
+                return ConditionReglementService::CR30FDM15['dayEndMonth'];
             default:
                 return 'Aucune condition valable';
         }
